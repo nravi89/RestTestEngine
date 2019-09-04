12974411b0c348b6b2ee18a3c3db2e643e2cc688
nravi89 <n.ravi89@gmail.com>
2019-09-04 09:53:55 +0530

    properties

--- src/main/java/com/rf/util/Test.java
+++ src/main/java/com/rf/util/Test.java
@@ -1,5 +1,9 @@
 package com.rf.util;
 
+import com.github.javafaker.Faker;
+import com.github.javafaker.HarryPotter;
+import com.github.javafaker.Name;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
@@ -7,14 +11,29 @@ public class Test {
 	//http://localhost:8181/SpringExamples/get
 	
 	public static void main(String[] args) {
-		Response response = RestAssured.given()
+		/*Response response = RestAssured.given()
 		          .baseUri("http://localhost:8181")
 		                          .basePath("{name}/SpringExamples/{name}")
 		                          .pathParam("name", "get")
 		          		          .log().all().get();
 		
-		System.out.println(response);
+		System.out.println(response);*/
+		Faker faker = new Faker();
+        Name name = faker.name();
+		String fullName = name.fullName(); // Miss Samanta Schmidt
+		String firstName = name.firstName(); // Emory
+		String lastName = name.lastName(); // Barton
+
+		String streetAddress = faker.address().fullAddress(); // 60018 Sawayn Brooks Suite 449
 		
+		System.out.println("name:"+fullName);
+		System.out.println("firstName:"+firstName);
+		System.out.println("last Name:"+lastName);
+		System.out.println("address:"+streetAddress);
+		
+		
+		HarryPotter harryPotter = faker.harryPotter();
+		System.out.println(harryPotter.character()+":"+harryPotter.quote());
 	}
 
 }
